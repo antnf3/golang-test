588fcb7c143108498f75ccfe289b1f28c2c5cc96
antnf3 <antnf3@gmail.com>
2020-07-16 22:15:14 +0900

    url checker

---
+++ myDict/dict.go
@@ -0,0 +1,46 @@
+package myDict
+
+import "errors"
+
+var (
+	errNotFound   = errors.New("NotFound")
+	errCantUpdate = errors.New("Cant update non-existing word")
+	errWordExists = errors.New("That word already exists")
+)
+
+// Dictionary type
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = word
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
