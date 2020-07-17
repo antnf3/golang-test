89e2d52144632a0b2f88da281e63901400cd5f2a
antnf3 <antnf3@gmail.com>
2020-07-17 21:32:35 +0900

    Goroutines, channel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 노마드코드의 Go 따라하기
+
+Goroutines
+
+channel
--- main.go
+++ main.go
@@ -3,43 +3,61 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 var errRequestFaield = errors.New("Request Failed")
 
+// func main() {
+// 	var results = make(map[string]string)
+// 	urls := []string{
+// 		"https://www.airbnb.com/",
+// 		"https://www.google.com/",
+// 		"https://www.amazon.com/",
+// 		"https://www.reddit.com/",
+// 		"https://www.google.com/",
+// 		"https://soundcloud.com/",
+// 		"https://www.facebook.com/",
+// 		"https://www.instagram.com/",
+// 		"http://academy.nomadcoders.co/",
+// 	}
+// 	for _, url := range urls {
+// 		result := "OK"
+// 		err := hitURL(url)
+// 		if err != nil {
+// 			result = "FAILED"
+// 		}
+// 		results[url] = result
+// 	}
+// 	for url, result := range results {
+// 		fmt.Println(url, result)
+// 	}
+// }
+
+// func hitURL(url string) error {
+// 	fmt.Println("Checking: ", url)
+// 	resp, err := http.Get(url)
+// 	if err != nil || resp.StatusCode >= 400 {
+// 		fmt.Println(err, resp.Status)
+// 		return errRequestFaield
+// 	}
+// 	return nil
+// }
+
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"http://academy.nomadcoders.co/",
+	c := make(chan bool)
+	people := [2]string{"min", "hyun"}
+
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	result := <-c
+	result1 := <-c
+	fmt.Println(result)
+	fmt.Println(result1)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.Status)
-		return errRequestFaield
-	}
-	return nil
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	c <- true
 }
